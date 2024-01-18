b78e5ecc4bd0f0b2d645720bff731c99c1b7b430
Anastasia <shakirova.2797@mail.ru>
2024-01-18 22:42:47 +0600

    first

---
+++ index.js
@@ -0,0 +1,35 @@
+$('.slider-nav').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  
+
+let modal = document.getElementById('form-window')
+function showFirstAdapt(){
+    modal.classList.add('active-form')
+}
+function closeFirstAdapt(){
+    modal.classList.remove('active-form')
+}
